3c938a7c181f834c46ab4006dd5559e2f98cc56f
eric <eagle2283429@yahoo.com.tw>
2024-03-26 16:09:05 +0800

    Initial Commit

---
+++ PagerLoop/PagerLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagerLoopApp.swift
+//  PagerLoop
+//
+//  Created by you chou on 2024/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PagerLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
